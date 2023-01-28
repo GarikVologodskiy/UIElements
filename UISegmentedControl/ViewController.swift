554123a206bbbfbf4529ce234eeece4c381b2786
Harry Vologodskii <49877717+GarikVologodskiy@users.noreply.github.com>
2023-01-28 15:31:00 +0500

    Initial Commit

---
+++ UISegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by Igor Vologodskiy on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
